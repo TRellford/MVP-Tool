bb734fb49672774d638969889787baeb6bea3d9e
TRellford <tysonn.rellford@gmail.com>
2025-02-23 20:19:37 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,8 +11,6 @@ BALL_DONT_LIE_API_URL = "https://api.balldontlie.io/v1/games"
 
 # ✅ Cache Data for Efficiency
 API_KEY = "aa93bed3-e51f-48c5-bfad-74d85cee2c72"  # ⬅️ Replace this with your real key
-
-@st.cache_data(ttl=3600)
 def get_nba_games(date):
     """Fetch NBA games from BallDontLie API for a specific date."""
     if isinstance(date, str):
